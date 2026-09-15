4c2ec1018b90ab1713af192ad61b969b1bf9a884
Illa Tsiluiko <helperfor@ukr.net>
2026-09-15 16:22:13 +0300

    V1.0 - add redis to products and products_category_list

--- newdjango/settings.py
+++ newdjango/settings.py
@@ -115,6 +115,16 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://redis:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
--- products/views.py
+++ products/views.py
@@ -4,6 +4,8 @@ from .permissions import IsAuthorOrAdmin
 from .serializers import ProductReadSerializer, ProductWriteSerializer, CategorySerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import mixins, viewsets
+from django.core.cache import cache
+from rest_framework.response import Response
 
 class ProductViewSet(ModelViewSet):
     # queryset = Product.objects.all()
@@ -16,7 +18,14 @@ class ProductViewSet(ModelViewSet):
         return ProductReadSerializer
 
     def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
+        product = serializer.save(author=self.request.user)
+
+        cache.delete("products:list")
+
+        if product.category:
+            cache.delete(
+                f"products:category:{product.category.slug}"
+            )
 
     def get_queryset(self):
         queryset = super().get_queryset()
@@ -32,6 +41,36 @@ class ProductViewSet(ModelViewSet):
             queryset = queryset.filter(category__slug=category_slug)
 
         return queryset
+
+    def list(self, request, *args, **kwargs):
+        category_slug = request.query_params.get("category")
+
+        if category_slug:
+            cache_key = f"products:category:{category_slug}"
+        else:
+            cache_key = "products:list"
+
+        cached_data = cache.get(cache_key)
+
+        if cached_data is not None:
+            return Response(cached_data)
+
+        queryset = self.get_queryset()
+
+        serializer = self.get_serializer(
+            queryset,
+            many=True
+        )
+
+        data = serializer.data
+
+        cache.set(
+            cache_key,
+            data,
+            timeout=60 * 5
+        )
+
+        return Response(data)
 
 
 class CategoryViewSet(
